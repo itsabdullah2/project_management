52afb573c70ee0cabb4a3d2115039f3c49ca0f5c
itsabdullah2 <abdullahosman3020@gmail.com>
2024-08-19 23:08:48 +0300

    init FilterItems component

---
+++ src/components/app/FilterItems.jsx
@@ -0,0 +1,17 @@
+const FilterItems = () => {
+  return (
+    <div className="mt-20 px-6">
+      <div className="flex items-center justify-between">
+        <div>Priority</div>
+
+        <div className="flex items-center gap-4">
+          <div>Sort</div>
+          <div>More Filter</div>
+          <div>+ Add Task</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterItems;
