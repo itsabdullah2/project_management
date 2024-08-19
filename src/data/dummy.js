a43594e0974a1ad2898d02adf38b53d4dbdb9d6a
itsabdullah2 <abdullahosman3020@gmail.com>
2024-08-19 23:15:46 +0300

    add new shared styles

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -6,3 +6,6 @@ export const hoverStyling =
 
 export const styleOfLogo =
   "block w-12 h-12 rounded-full bg-purple relative before:absolute before:w-8 before:h-8 before:bg-darkGray before:rounded-full before:left-1/2 before:top-1/2 before:-translate-x-1/2 before:-translate-y-1/2";
+
+export const stylingPath =
+  "before:absolute before:left-0 before:-bottom-[30px] before:w-[2px] before:h-[25px] before:bg-gray after:absolute after:left-[2px] after:-bottom-[30px] after:w-[30px] after:h-[2px] after:bg-gray";
