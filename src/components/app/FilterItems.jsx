811e6cd52717e4882ad06aa688832eb14b2eddc1
itsabdullah2 <abdullahosman3020@gmail.com>
2024-08-20 16:10:26 +0300

    add some icons to filter bar

--- src/components/app/FilterItems.jsx
+++ src/components/app/FilterItems.jsx
@@ -1,13 +1,27 @@
+import { IoBookmarkOutline, IoFilter, IoAddOutline } from "react-icons/io5";
+import { FaSort } from "react-icons/fa";
+import { styleFilterSection } from "../../data/dummy";
+
 const FilterItems = () => {
   return (
-    <div className="mt-20 px-6">
+    <div className="mt-14">
       <div className="flex items-center justify-between">
-        <div>Priority</div>
+        <div className={`${styleFilterSection} border-lightGray`}>
+          <IoBookmarkOutline /> <span>Priority</span>
+        </div>
 
         <div className="flex items-center gap-4">
-          <div>Sort</div>
-          <div>More Filter</div>
-          <div>+ Add Task</div>
+          <div className={`${styleFilterSection} border-lightGray`}>
+            <FaSort />
+            <span>Sort</span>
+          </div>
+          <div className={`${styleFilterSection} border-lightGray`}>
+            <IoFilter />
+            <span>More Filter</span>
+          </div>
+          <div className={`${styleFilterSection} bg-purple border-purple`}>
+            <IoAddOutline /> <span>Add Task</span>
+          </div>
         </div>
       </div>
     </div>
