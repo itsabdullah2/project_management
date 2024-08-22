339f8532566a83a9495e8bb7df3d4bd072ba49f7
itsabdullah2 <abdullahosman3020@gmail.com>
2024-08-22 18:14:22 +0300

    add contextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AppStateProvider } from "./context/AppStateContext";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AppStateProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AppStateProvider>
 );
