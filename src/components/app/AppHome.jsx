33d7b0f2e9ee1922af28979854745e14fdf88b93
itsabdullah2 <abdullahosman3020@gmail.com>
2024-08-24 00:59:41 +0300

    add styling

--- src/components/app/AppHome.jsx
+++ src/components/app/AppHome.jsx
@@ -2,7 +2,7 @@ import { AppNavbar, FilterItems } from "../../";
 
 const AppHome = () => {
   return (
-    <div className="h-screen bg-veryDarkGray text-offWhite flex-1">
+    <div className="h-screen bg-veryDarkGray text-offWhite flex-1 px-6">
       <AppNavbar />
       <FilterItems />
     </div>
