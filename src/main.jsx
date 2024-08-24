eace36848532c07fb74a53cb03393c53c5268bce
itsabdullah2 <abdullahosman3020@gmail.com>
2024-08-25 00:40:31 +0300

    wrap the app with the provider of the redux store

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { AppStateProvider } from "./context/AppStateContext";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
   <AppStateProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </AppStateProvider>
 );
