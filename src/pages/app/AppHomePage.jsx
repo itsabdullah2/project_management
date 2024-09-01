d3e5cbceb4b7d7ee6f489ea742b36f9b3ae436f4
itsabdullah2 <abdullahosman3020@gmail.com>
2024-09-01 18:56:32 +0300

    work on responsiveness and content

--- src/pages/app/AppHomePage.jsx
+++ src/pages/app/AppHomePage.jsx
@@ -2,7 +2,7 @@ import { AppHome, Sidebar } from "../../";
 
 const AppHomePage = () => {
   return (
-    <div className="flex">
+    <div className="grid grid-cols-12">
       <Sidebar />
       <AppHome />
     </div>
