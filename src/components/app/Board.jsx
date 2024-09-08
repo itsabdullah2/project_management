5dbf38bc0a753ac3390e4d41126e457e71a5a58e
itsabdullah2 <abdullahosman3020@gmail.com>
2024-09-08 11:17:05 +0300

    import TasksHolder component

--- src/components/app/Board.jsx
+++ src/components/app/Board.jsx
@@ -1,10 +1,9 @@
-import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import {
   useAppState,
   AppNavbar,
   FilterItems,
-  TaskForm,
-  CalendarTaskPopup,
+  TasksHolder,
+  AddTaskPopup,
 } from "../../";
 
 const Board = () => {
@@ -19,25 +18,9 @@ const Board = () => {
       <AppNavbar />
       <FilterItems />
       <div className={`grid grid-cols-10 gap-2 mt-5`}></div>
-      {isPopupOpen && <CalendarTaskPopup />}
+      {isPopupOpen && <AddTaskPopup />}
       <div className="grid grid-cols-10 gap-2">
-        <div className="col-span-2 bg-darkGray p-4 rounded-lg flex flex-col">
-          <div className="flex items-center justify-between text-offWhite mb-3">
-            <h3 className="text-lg font-normal select-none">To do</h3>
-            <span className="cursor-pointer">
-              <HiOutlineDotsHorizontal size={22} />
-            </span>
-          </div>
-          <div className="flex-1 flex flex-col gap-2">
-            <TaskForm />
-          </div>
-          <button
-            type="button"
-            className="bg-gray text-center mt-3 rounded-md py-3 text-offWhite"
-          >
-            + Add New Task
-          </button>
-        </div>
+        <TasksHolder />
       </div>
     </div>
   );
