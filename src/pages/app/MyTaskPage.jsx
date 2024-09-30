f1930a1c686fa9674e27bc516d11d97ad70d55fa
itsabdullah2 <abdullahosman3020@gmail.com>
2024-09-30 16:15:52 +0300

    edit the css classes

--- src/pages/app/MyTaskPage.jsx
+++ src/pages/app/MyTaskPage.jsx
@@ -2,7 +2,7 @@ import { Sidebar, MyTask } from "../../";
 
 const MyTaskPage = () => {
   return (
-    <div className="grid grid-cols-12">
+    <div className="grid grid-cols-12 relative">
       <Sidebar />
       <MyTask />
     </div>
