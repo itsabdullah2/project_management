1ea665574da5fc55a0ecbd461330c8ff476fa434
itsabdullah2 <abdullahosman3020@gmail.com>
2024-09-30 16:09:49 +0300

    render the tasks inside the task holder depending on the topic & edit the content

--- src/components/app/TasksHolder.jsx
+++ src/components/app/TasksHolder.jsx
@@ -1,34 +1,39 @@
-import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { IoTrashOutline } from "react-icons/io5";
 import { TaskForm, useAppState, useAddTopic, useFormData } from "../../";
-import { useSelector } from "react-redux";
 
 const TasksHolder = () => {
-  const { topicsHolder } = useAddTopic();
+  const { topicsHolder, handleDeleteTasksHolder } = useAddTopic();
   const { handleOpenAndClose, setIsPopupOpen, setSelectedTopic } =
     useAppState();
   const { listOfTasks } = useFormData();
 
   return topicsHolder.length > 0
-    ? topicsHolder.map((topic, i) => (
+    ? topicsHolder.map((topic) => (
         <div
-          key={i}
-          className="col-span-2 bg-darkGray p-4 rounded-2xl flex flex-col"
+          key={topic.id}
+          className=" w-[390px] bg-darkGray p-4 rounded-2xl flex flex-col"
         >
           <div className="flex items-center justify-between text-offWhite mb-3">
-            <h3 className="text-lg font-normal select-none">{topic}</h3>
-            <span className="cursor-pointer">
-              <HiOutlineDotsHorizontal size={22} />
+            <h3 className="text-lg font-normal select-none">{topic.name}</h3>
+            <span
+              className="cursor-pointer active:translate-x-[2px] duration-200"
+              onClick={() => handleDeleteTasksHolder(topic.id)}
+            >
+              <IoTrashOutline size={22} />
             </span>
           </div>
           <div className="flex-1 flex flex-col gap-2 max-h-[600px] overflow-y-auto pr-2">
-            <TaskForm listOfTasks={listOfTasks[topic] || []} topic={topic} />
+            <TaskForm
+              listOfTasks={listOfTasks[topic.name] || []}
+              topic={topic.name}
+            />
           </div>
           <button
             type="button"
-            className="bg-gray text-center mt-3 rounded-xl py-3 text-offWhite duration-100 active:translate-x-[2px]"
+            className="bg-gray text-center mt-3 rounded-xl py-3 text-offWhite duration-100 active:translate-x-[2px] select-none"
             onClick={() => {
               handleOpenAndClose(setIsPopupOpen);
-              setSelectedTopic(topic);
+              setSelectedTopic(topic.name);
             }}
           >
             + Add New Task
