30a57f982a4c68f2068b021123cfdee0cc17d019
itsabdullah2 <abdullahosman3020@gmail.com>
2024-09-30 16:07:07 +0300

    handle the UI of the task and display items from MUI

--- src/components/app/TaskItem.jsx
+++ src/components/app/TaskItem.jsx
@@ -1,44 +1,43 @@
-import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import Checkbox from "@mui/material/Checkbox";
+import { IoTrashOutline } from "react-icons/io5";
 import { useAppState } from "../../";
 
 const TaskItem = () => {
-  const { isOptionsOpen, setIsOptionsOpen, handleOpenAndClose } = useAppState();
+  const { setIsOptionsOpen, handleOpenAndClose } = useAppState();
 
   return (
     <div className="bg-gray py-4 px-5 col-span-3 rounded-xl relative">
       <div className="flex items-center justify-between text-offWhite">
         <div className="flex items-center gap-2">
-          <input type="checkbox" id="task" name="task" />
+          <Checkbox
+            sx={{
+              color: "#4f46e5",
+              "&.Mui-checked": {
+                color: "#4f46e5",
+              },
+              padding: "0",
+            }}
+          />
           <label htmlFor="task">Your task here</label>
         </div>
-        <span onClick={() => handleOpenAndClose(setIsOptionsOpen)}>
-          <HiOutlineDotsHorizontal
-            size={20}
-            className="cursor-pointer active:translate-x-[2px] duration-200"
-          />
+        <button
+          type="button"
+          onClick={() => handleOpenAndClose(setIsOptionsOpen)}
+          className="cursor-pointer active:translate-x-[2px] duration-200"
+        >
+          <IoTrashOutline />
+        </button>
+      </div>
+      <div className="flex items-center justify-between mt-3">
+        <div className="text-sm text-purple text-semibold self-end">
+          08:30 <span className="text-offWhite mx-1">To</span> 09:20
+        </div>
+        <span className="bg-darkGray py-1 px-2 rounded-md text-offWhite text-sm">
+          {/*TODO change the color of the text */}
+          High
         </span>
       </div>
-      <div className="my-2 text-sm text-purple text-semibold">
-        08:30 <span className="text-offWhite mx-1">To</span> 09:20
-      </div>
-      <span className="bg-high py-1 px-2 rounded-md text-white text-sm">
-        High
-      </span>
-      {/* Popup Options */}
-      {isOptionsOpen && (
-        <div
-          className={`absolute -right-[150px] top-[30px] rounded-lg py-3 px-4 bg-gray w-36 origin-bottom-left`}
-        >
-          <div className="flex flex-col">
-            <button className="text-offWhite w-full text-left text-lg pb-2 border-b border-darkGray font-semibold">
-              Edit
-            </button>
-            <button className="text-offWhite w-full text-left text-lg pt-2 font-semibold">
-              Delete
-            </button>
-          </div>
-        </div>
-      )}
+      {/* Coming Soon => Button to edit the task */}
     </div>
   );
 };
